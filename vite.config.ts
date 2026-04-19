04fa3f3618daa51e930a27cc0789225a6b69b259
Fivel <fivel10@hotmail.com>
2026-04-19 15:53:42 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/tenkuifestivalslp/", // <--- ESTA ES LA LÍNEA CLAVE
   server: {
     host: "::",
     port: 8080,
